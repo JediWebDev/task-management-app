5239628fefe3e083e345376c2335287271c2dd02
Andrew <56172745+JediWebDev@users.noreply.github.com>
2025-03-14 20:44:55 -0700

    Update Navbar.js

--- task-management-app/src/components/Navbar.js
+++ task-management-app/src/components/Navbar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-500 text-white p-3 flex gap-4">
+    <nav className="bg-blue-500 text-white p-3 flex gap-4 dark:bg-gray-900">
       <Link href="/" className="hover:underline">Home</Link>
       <Link href="/contact" className="hover:underline">Contact</Link>
       <Link href="/about" className="hover:underline">About</Link>
